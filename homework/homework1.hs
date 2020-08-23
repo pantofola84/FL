0acfa892d4177157c7520f6e0f92237cc156f3ba
NV <niccolo.vettorello@zextras.com>
2020-08-23 18:37:03 +0200

    Advanced with homework 1.

--- homework/homework1.hs
+++ homework/homework1.hs
@@ -13,9 +13,33 @@ toDigits n
  | n > 0 = toDigits (n `div` 10) ++ [n `mod` 10]
  | otherwise = []
 
+
 -- toDigitsRev
 -- should do the same, but with digits reversed
 -- E: toDigitsRev 317 = [7, 1, 3]
 
 toDigitsRev :: Integral a => a -> [a]
 toDigitsRev a = reverse (toDigits a)
+
+
+-- doubleAlternate
+-- doubles values in the list on the base of the parameter passed
+-- E: doubleAlternate 1 [1, 2, 3] = [1, 4, 3]
+-- E: doubleAlternate 2 [1, 4] = [2, 4]
+
+doubleAlternate :: (Eq a, Num a, Num b) => a -> [b] -> [b]
+doubleAlternate _ [] = []
+doubleAlternate 1 (f : r) = [f] ++ doubleAlternate 2 r
+doubleAlternate 2 (f : r) = [2 * f] ++ doubleAlternate 1 r 
+
+
+-- doubleEveryOther
+-- should double every number at a pair index, beginning to count from the right
+-- E: doubleEveryOther [2, 4, 5, 2] = [4, 4, 10, 2]
+-- E: doubleEveryOther [] = []
+-- E: doubleEveryOther [2, 4, 6] = [2, 8, 6]
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther l
+ | ((length l) `mod` 2) == 0 = doubleAlternate 2 l
+ | otherwise = doubleAlternate 1 l  
