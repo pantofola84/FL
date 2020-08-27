5addfaa0dc1810b2c52847ed3230093e73fd11a8
NV <niccolo.vettorello@zextras.com>
2020-08-27 11:51:51 +0200

    Completed homework 1.

--- homework/homework1.hs
+++ homework/homework1.hs
@@ -8,7 +8,7 @@ module Validate where
 -- NOTE: the negative argument MUST be passed in brackets, because (-) is a function
 -- E: toDigits (-23) = []
 
-toDigits :: Integral a => a -> [a]
+toDigits :: Integer -> [Integer]
 toDigits n
  | n > 0 = toDigits (n `div` 10) ++ [n `mod` 10]
  | otherwise = []
@@ -18,7 +18,7 @@ toDigits n
 -- should do the same, but with digits reversed
 -- E: toDigitsRev 317 = [7, 1, 3]
 
-toDigitsRev :: Integral a => a -> [a]
+toDigitsRev :: Integer -> [Integer]
 toDigitsRev a = reverse (toDigits a)
 
 
@@ -42,4 +42,23 @@ doubleAlternate 2 (f : r) = [2 * f] ++ doubleAlternate 1 r
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther l
  | ((length l) `mod` 2) == 0 = doubleAlternate 2 l
- | otherwise = doubleAlternate 1 l  
+ | otherwise = doubleAlternate 1 l
+
+
+-- sumDigits
+-- should return the sum of the whole list. Numbers composed by two or more digits are decomposed i
+-- n single digits
+-- E: sumDigits [2, 23, 3] = 10
+
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (f : r) = sum (toDigits f) + sumDigits r
+
+
+-- validate
+-- should return true if an integer is a valid credit card number, false otherwise
+-- E: validate 4012888888881881 = True
+-- E: validate 4012888888881882 = False
+
+validate :: Integer -> Bool
+validate i = if ((sumDigits.doubleEveryOther.toDigits) i) `mod` 10 == 0 then True else False
