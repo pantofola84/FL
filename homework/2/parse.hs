44f280ce1ed1f268cce0dcf4118d1b4addaea28a
pantofola84 <vettorelloniccolo97@gmail.com>
2020-09-18 16:26:00 +0200

    Completed function parse.

--- homework/2/parse.hs
+++ homework/2/parse.hs
@@ -14,18 +14,23 @@ import Data.Char
 import Text.Read
 
 
+-- checks if a trimmed string has the correct length, has a flag to know if it's dealing with an error
+validLength :: [String] -> Bool -> Bool
+validLength t True 	= length t >= 4
+validLength t False 	= length t >= 3
+
 -- given a string, returns true if it's a valid timestamp
 isValidTimeStamp :: String -> Bool
-isValidTimeStamp [] = False
-isValidTimeStamp [c] = isDigit c
-isValidTimeStamp (f : r) = isValidTimeStamp [f] && isValidTimeStamp r
+isValidTimeStamp [] 		= False
+isValidTimeStamp [c] 		= isDigit c
+isValidTimeStamp (f : r) 	= isValidTimeStamp [f] && isValidTimeStamp r
 
 
 -- given a Maybe, is true if it represents a valid error priority
-readAndValidate :: Maybe a -> Maybe a -> Bool
-readAndValidate inf_constr sup_constr = do
- i <- inf_constr
- s <- sup_constr
+readAndValidate :: (Ord a, Num a) => Maybe a -> Bool
+readAndValidate x = case x of
+ Nothing 	-> False
+ Just a 	-> (a < 101) && (a > 0)
 
 -- given a trimmed string, returns true if it is a valid Info LogMessage
 isInfo :: [String] -> Bool
@@ -41,16 +46,16 @@ isWarning s = ((s !! 0) == "W") && (isValidTimeStamp (s !! 1))
 isError :: [String] -> Bool
 isError s = ((s !! 0) == "E")	&&
  (isValidTimeStamp (s !! 2))	&&
- readAndValidate (readMaybe (s !! 1)::Int) (readMaybe (s !! 1)::Int)
+ readAndValidate (readMaybe (s !! 1)::Maybe Int)
 
 
 -- given a string, returns the corresponding LogMessage
 parse :: String -> LogMessage
 parse s
- | isError trimmedS 	= LogMessage (Error (read (trimmedS !! 1)::Int)) (read (trimmedS !! 2)::Int) (unwords (drop 3 trimmedS))
- | isWarning trimmedS 	= LogMessage Warning (read (trimmedS !! 1)::Int) (unwords (drop 2 trimmedS))
- | isInfo trimmedS 	= LogMessage Info (read (trimmedS !! 1)::Int) (unwords (drop 2 trimmedS))
- | otherwise		= Unknown s
+ | validLength trimmedS True && isError trimmedS		= LogMessage (Error (read (trimmedS !! 1)::Int)) (read (trimmedS !! 2)::Int) (unwords (drop 3 trimmedS))
+ | validLength trimmedS False && isWarning trimmedS		= LogMessage Warning (read (trimmedS !! 1)::Int) (unwords (drop 2 trimmedS))
+ | validLength trimmedS False && isInfo trimmedS		= LogMessage Info (read (trimmedS !! 1)::Int) (unwords (drop 2 trimmedS))
+ | otherwise						= Unknown s
  where trimmedS = words s
 
 
